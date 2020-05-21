bfbe118963dcc1dc94bbc7d0e17f89ddc461adf8
sruli <sruli@saurymper.com>
2020-05-21 22:44:01 +0100

    restructure - beta removed almost all reliance of subprocess and instead imported fail2ban python modules directly beta version still needs testing

---
+++ central-f2b/f2bmods.py
@@ -0,0 +1,129 @@
+# suuid - System UUID
+def suuid():
+    try:
+        with open("/etc/machine-id", 'r') as uuid_file:
+            h_uuid = uuid_file.read().strip()
+    except FileNotFoundError:
+        import uuid
+        h_uuid = uuid.uuid1().hex
+        with open("/etc/machine-id", 'a') as uuid_file:
+            uuid_file.write(h_uuid)
+    suuid.uuid = h_uuid
+    suuid.id = h_uuid[0:5] + "_" + h_uuid[27:32]
+    suuid.col_id = ("host_test"+suuid.id)
+
+# parg - Passed Arguments
+def parg():
+    import sys
+    import ipaddress
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    if sys.argv[0] not in "removeip.py":
+        parser.add_argument(
+            '-j', action="store", dest="jailname"
+        )
+        parser.add_argument(
+            '-p', action="store", dest="port"
+        )
+        parser.add_argument(
+            '-pr', action="store", dest="protocol"
+        )
+        parser.add_argument(
+            '-d', action="store", dest="dest_ip"
+        )
+        parser.add_argument(
+            '-i', action="store", dest="ip"
+        )
+    else:
+        # The following are only in use for removeip.py
+        arg_msg="Example - 'python3 /root/removeip.py -i 192.168.1.1 -t 1'. \nFor -t type arg use 1 for permenant whitelist, 2 for remove ban only"
+        parser.add_argument(
+            '-i', type=ipaddress.ip_address, action="store", dest="ip", required=True, help=arg_msg
+        )
+        parser.add_argument(
+            '-t', type-int, action="store", dest="remove_type", required=True
+        )
+
+    args = parser.parse_args()
+
+    parg.ip = str(args.ip)
+
+    # The following are only in use for removeip.py
+    if sys.argv[0] not in "removeip.py":
+        parg.jn = args.jailname
+        parg.prt = args.protocol
+        parg.port = args.port
+        parg.d_ip = args.dest_ip
+    else:
+        parg.type = args.remove_type
+        if parg.type not in (1,2):
+            print("FAILED: "+ arg_msg)
+            sys.exit(1)
+    # End of removeip.py requirements
+
+
+# ncol - New Column (add System UUID + New column for this system to DB)
+def ncol(cursor, db, my_host_name):
+    suuid()
+    import mysql.connector
+    querycol = """
+    SELECT COUNT(*)
+    FROM host_table
+    WHERE host_uuid = '{}'
+    """.format(
+        suuid.uuid
+    )
+    cursor.execute(querycol)
+    result = cursor.fetchall()
+    exists = result
+    if (exists[0][0]) == 0:
+        try:
+            addhost = """
+            INSERT INTO host_table (
+                host_name,host_uuid,host_id
+            )
+            VALUES ('{}','{}','{}')
+            """.format(
+                my_host_name,suuid.uuid,suuid.id
+            )
+            cursor.execute(addhost)
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            # If the columnn does not exist and we cannot add it exit
+            db.rollback()
+            sys.exit(1)
+
+    try:
+        # Add column in ip_table for this host
+        addcol = """
+        ALTER TABLE ip_table
+        ADD COLUMN {0}
+        SMALLINT
+        NOT NULL
+        DEFAULT 0,
+        ADD INDEX ({0},whitelist,created),
+        ADD INDEX (whitelist,{0}),
+        ADD INDEX ({0},created)
+        """.format(
+            suuid.col_id
+        )
+        cursor.execute(addcol)
+
+        # Set old records in ip_table to '5' for this host
+        setold = """
+        UPDATE ip_table
+        SET {} = '5'
+        WHERE DATE_SUB(CURDATE(),INTERVAL 25 DAY) >= created
+        """.format(
+            suuid.col_id
+        )
+        cursor.execute(setold)
+        db.commit()
+
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        # If the columnn does not exist and we cannot add it exit
+        db.rollback()
+        sys.exit(1)
---
+++ central-f2b/removeip.py
@@ -0,0 +1,65 @@
+#usage call with "-i <ip> -t <type>" (type = 1 for permenant whitelist, 2 for remove ban only)
+# "python3 /root/removeip.py -i 192.168.1.1 -t 1" it type option not passed default is 2
+import sys
+import os
+import socket
+import subprocess
+import ipaddress
+#import sqlite3
+import mysql.connector
+import lc_myconn as my_conn
+from f2bmods import suuid, parg
+from fail2ban.client.csocket import CSocket
+
+my_host_name = socket.gethostname()
+
+# mysql connection
+db = mysql.connector.connect(
+    host=(my_conn.host),
+    port=(my_conn.port),
+    user=(my_conn.user),
+    passwd=(my_conn.passwd),
+    db=(my_conn.db)
+)
+
+def main():
+    suuid()
+    parg()
+    # Open a socket to fail2ban
+    s = CSocket("/run/fail2ban/fail2ban.sock")
+    s.send(["unban", rem.ip])
+#    f2bcmd1 = ("fail2ban-client unban " + parg.ip)
+#    subprocess.run(f2bcmd1, shell=True)
+    # If type is permenant unban add IP to ignore list
+    if parg.type == 1:
+        # Add to f2b live ignoreip (f2b wont read whitelist file until reload)
+        # Get a list of jails to execute ignoreip on
+        jails = s.send(["status"])[1][1][1]
+        jails = jails.split(", ")
+        for jname in jails:
+            # f2bcmd2 = ("fail2ban-client set " + row[0] + " addignoreip " + parg.ip)
+            # subprocess.run(f2bcmd2, shell=True)
+            s.send(['set', jname, 'addignoreip', rem_ip])
+    s.close()
+
+    db.ping(reconnect=True, attempts=3, delay=150)
+    cursor = db.cursor()
+    update = """
+    UPDATE ip_table
+    SET whitelist = '{1}', {0} = '4'
+    WHERE ip = '{2}'
+    """.format(
+        suuid.col_id, parg.type, parg.ip
+    )
+    cursor.execute(update)
+    db.commit()
+    if parg.type == 1:
+        with open("/etc/fail2ban/jail.d/whitelist.local", "r+") as whitelist:
+            if parg.ip not in whitelist.read():
+                whitelist.write(' {}\n'.format(parg.ip,))
+
+    db.close()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
