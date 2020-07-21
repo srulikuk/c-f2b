b5c86ff1823a2494a30d93c6e4a6266549c3553d
sruli <sruli@saurymper.com>
2020-07-21 18:21:37 +0100

    update added more iprules types (source ip, source port)

--- iptables/rules.sh
+++ iptables/rules.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-# Insert wan interfaces (this is normally inserted by the setup script)
-#interface+=("")
 
-# Script to get list of open ports from iptables
-
-# IMPORTANT: This will NOT work for all machines as the
-# output of iptables is not formatted exactly the same.
-
-# This script is also used to test without actually making
-# any chnages to iptables "test_mode=y" for this mode  the
-# var "test_pass=n" is in use.  When sourcing this  script
-# ensure you first set test_mode="y"
-
-# This script ONLY works when using iptables to specify ports
-# If you use "exclude <port> ACCEPT" (! --dport 123 -j ACCEPT) or
-# "include <port> REJECT (--dport 123 -j REJECT)" do NOT use this
-# script!
-# Its important to run this as soon as a new port is opened else
-# the connecting ip will get banned by fail2ban as soon as it tries
-# to connect to the port.
+interface+=("ens3")
+interface+=("ens9")
 
 auto_save="n" # change to "y" if "netfilter-persistent save" should
 # be executed on each run without prompting. USE EXTREME CAUTION
@@ -38,6 +21,46 @@ delRules()
 			iptables -D INPUT "$rule"
 		done
 	fi
+}
+
+getPorts()
+{
+	if [[ $test_mode == y ]] ; then
+		if [[ $p == tcp ]] ; then
+			printf '\nChecking TCP ports...\n'
+		else
+			printf '\nChecking UDP ports...\n'
+		fi
+	fi
+
+	mapfile -t tmp_list < <(iptables-save | grep -E "($r_0) ($r_1).*($r_2).*${p}.*--dport.*" | grep -Ev "$grep_v")
+	local rule
+	for rule in "${tmp_list[@]}" ; do
+		# check if rule has a iface which is not in list of wan interfaces
+		if [[ $rule =~  -i && ! $rule =~ (${tmp_iface::-1}) ]] ; then
+			continue
+			# check if rule has source ip
+		elif [[ $rule =~ (-s |--src-range |--sport) ]] ; then
+			local ip
+			ip="$(sed -n 's/.*\(-s\|--src-range\)\ \([^ ]*\) .*/\2/p' <<< "$rule")"
+			[[ -n $ip ]] && ip="ip=$ip "
+			local sport
+			sport="$(sed -n 's/.*--sport\ \([^ ]*\) .*/\1/p' <<< "$rule")"
+			[[ -n $sport ]] && sport="sport=$sport "
+			local port
+			dport="$(sed -n 's/.*--dports\{0,1\}\ \([^ ]*\) .*/\1/p' <<< "$rule")"
+			rule="$(printf '%s%sdport=%s' "$ip" "$sport" "$dport")"
+			local opt_r
+			opt_r+=("$rule")
+		else
+			rule="$(sed -n 's/.*--dports\{0,1\}\ \([^ ]*\) .*/\1/p' <<< "$rule")"
+			local port_r
+			port_r+="${rule},"
+		fi
+
+	done
+	mapfile -t opt_rule < <(printf '%s\n' "${opt_r[@]}" | uniq)
+	port_list=( $(printf '%s' "$port_r" | tr ',' '\n' | sort -n | uniq) )
 }
 
 # create ports and ranges list from the raw ports list
@@ -71,7 +94,7 @@ checkPorts()
 			# test if its a valid port number
 			if ! [[ $port =~ ^([0-9]{1,5})$ ]] || ((port > 65535)) ; then
 				if [[ $test_mode = y ]] ; then
-					printf '\Got an invalid port number "%s"\nThe full output for ports was;\nTCP: %s\nUDP: %s\n' "$port" "${tcp_list[@]}" "${udp_list[@]}"
+					printf '\Got an invalid port number "%s"\nThe output for the ports was;\n%s\n' "$port" "${port_list[@]}"
 					test_pass="n"
 					break
 				else
@@ -140,16 +163,60 @@ addRules()
 {
 	if [[ $test_mode != y ]] ; then # extra check not to run in test_mode
 		# Set the dport param based on if single or multiport
-		local port_param
-		if [[ "${rules[r]}" == *","* ]] ; then
-				port_param[0]='-m'
-				port_param[1]='multiport'
-				port_param[2]='!'
-				port_param[3]='--dports'
-		else
-				port_param[0]='!'
-				port_param[1]='--dport'
+		local proto="$p"
+		local src_ip
+		local src_prt
+		local dst_opt
+		local d_port
+		local params
+		if [[ "$r" == *"ip="* ]] ; then
+			local s_ip
+			s_ip="$(sed -n 's/.*ip=\([^ ]*\) .*/\1/p' <<< "$r")"
+			if [[ $s_ip == *"-"* ]] ; then
+				src_ip[0]='-m'
+				src_ip[1]='iprange'
+				src_ip[2]='!'
+				src_ip[3]='--src-range'
+				src_ip[4]="$s_ip"
+			else
+				src_ip[0]='!'
+				src_ip[1]='-s'
+				src_ip[2]="$s_ip"
+			fi
 		fi
+		if [[ "$r" == *"sport="* ]] ; then
+			s_port="$(sed -n 's/.*sport=\([^ ]*\) .*/\1/p' <<< "$r")"
+			if [[ -z $src_ip ]] ; then
+				src_prt[0]='!'
+				src_prt[1]='--sport'
+				src_prt[2]="$s_port"
+			else
+				src_prt[0]='--sport'
+				src_prt[1]="$s_port"
+			fi
+		fi
+		if [[ "$r" == *"dport="* ]] ; then
+			r="$(sed -n 's/.*dport=\([^ ]*\)$/\1/p' <<< "$r")"
+		fi
+		if [[ "$r" == *","* ]] ; then
+			dst_opt[0]='-m'
+			dst_opt[1]='multiport'
+			if [[ -z $src_ip && -z $src_prt ]] ; then
+				dst_opt[2]='!'
+				dst_opt[3]='--dports'
+			else
+				dst_opt[2]='--dports'
+			fi
+		else
+			if [[ -z $src_ip && -z $src_prt ]] ; then
+				dst_opt[0]='!'
+				dst_opt[1]='--dport'
+			else
+				dst_opt[0]='--dport'
+			fi
+		fi
+		d_port="$r"
+		params=("$proto" "${src_ip[@]}" "${src_prt[@]}" "${dst_opt[@]}" "$d_port")
 
 		# Add the iptable rule for each interface
 		local iface
@@ -159,30 +226,31 @@ addRules()
 				local s_iface="($iface) "
 			fi
 
-			iptables -A INPUT -i "$iface" -m state --state NEW -p "$p" "${port_param[@]}" "${rules[r]}" -j \
-			LOG --log-prefix "PortProbe Rule: " --log-level 4 -m comment --comment \
-			"${p^^} ${s_iface}RULE # $((rule_num++)) of $total_rules port-probing logging"
+			 iptables -A INPUT -i "$iface" -m state --state NEW -p "${params[@]}" -j \
+			 LOG --log-prefix "PortProbe Rule: " --log-level 4 -m comment --comment \
+			 "PortProbe Rule: ${p^^} ${s_iface}RULE # $((rule_num++)) of $total_rules"
+# 			echo "iptables -A INPUT -i $iface -m state --state NEW -p ${params[@]} -j \
+# LOG --log-prefix 'PortProbe Rule: ' \
+# portprobe rule: ${p^^} ${s_iface}RULE # $((rule_num++)) of $total_rules"
+
 		done
 	fi
 }
 
-runScript() # in a function so it can be sourced
+runScript()
 {
 	shopt -q extglob || shopt -s extglob # turn on extglob
-
-	# Check for forward rules originating from wan interfaces - add to regex
-	forwd=$(printf 'FORWARD -i %s\\|' "${interface[@]}")
-
-	# Get TCP ports to array
-	mapfile -t tcp_list < <(iptables-save | grep -Ev 'PortProbe Rule: |RELATED|ESTABLISHED' | sed -n "/INPUT\|PREROUTING\|${forwd::-2}/s/.*-p tcp.*--dports\{0,1\} \([^ ]*\) -.*/\1/p" | tr ',' '\n' |sort -n | uniq)
-
-	# Get UDP ports to array
-	mapfile -t udp_list < <(iptables-save | grep -Ev 'PortProbe Rule: |RELATED|ESTABLISHED' | sed -n "/INPUT\|PREROUTING\|${forwd::-2}/s/.*-p udp.*--dports\{0,1\} \([^ ]*\) -.*/\1/p" | tr ',' '\n' |sort -n | uniq)
 
 	# Delete existing portprobe rules
 	if ! [[ $test_mode == y ]] ; then
 		delRules
 	fi
+
+	r_0='^-A|-I' # start match
+	r_1='INPUT|FORWARD|PREROUTING' # chain name
+	r_2='-i |-p |-s |--src-range' # rule params
+	grep_v="PortProbe Rule: |RELATED|ESTABLISHED"
+	tmp_iface=$(printf -- '-i %s|' "${interface[@]}")
 
 	# Check if the interfaces include a '+' if yes set for only 1 interface
 	for i in "${interface[@]}"; do
@@ -194,39 +262,34 @@ runScript() # in a function so it can be sourced
 
 	# sort the ports and ranges (check for duplicates) and add to iptables
 	for p in tcp udp ; do
-		if [[ $test_mode == y ]] ; then
-			if [[ $p == tcp ]] ; then
-				printf '\nChecking TCP ports...\n'
-			else
-				printf '\nChecking UDP ports...\n'
-			fi
-		fi
+		unset opt_rule
+		unset port_list
+		getPorts
+
 		[[ $test_pass == n ]] && break
-		if [[ $p = tcp ]] ; then
-			range_=("${tcp_list[@]}")
-		else
-			range_=("${udp_list[@]}")
-		fi
+		range_=("${port_list[@]}")
 
 		checkPorts
 		[[ $test_pass == n ]] && break
 
-		if [[ $p = tcp ]] ; then
-			tcp_list=("${ranges[@]}")
-			list=("${tcp_list[@]}")
-		else
-			udp_list=("${ranges[@]}")
-			list=("${udp_list[@]}")
-		fi
+		port_list=("${ranges[@]}")
+		list=("${port_list[@]}")
 
 		maxRules
 		[[ $test_pass == n ]] && break
 
+		if [[ -n ${rules[@]} && -n ${opt_rule[@]} ]] ; then
+			rules=("${rules[@]}" "${opt_rule[@]}")
+		fi
+		if [[ -n ${opt_rule[@]} && -z ${rules[@]} ]] ; then
+			rules=("${opt_rule[@]}")
+		fi
 
 		if [[ $test_mode == y ]] ; then
 
 			# create assoc array to store value if ranges are empty
 			declare -A empty
+
 			for r in "${rules[@]}" ;	do
 				if [[ -z ${rules[*]} ]] ; then
 					empty[$p]=1
@@ -241,9 +304,8 @@ runScript() # in a function so it can be sourced
 				fi
 			fi
 		else
-
 			total_rules=$((${#rules[@]} * ${#interface[@]})) # get total number of rules multiplied by the number of interfaces
-			for r in "${!rules[@]}" ;	do
+			for r in "${rules[@]}" ;	do
 				addRules
 			done
 
@@ -262,11 +324,11 @@ USE CAUTION when saving iptables rules!  [y/n] > '
 			fi
 
 			if [[ ${net_save,,} =~ ^(y|yes)$ || $auto_save = y ]] ; then
-				if ! [[ -f /etc/debian_version ]] ; then
+				if [[ -f /etc/debian_version ]] ; then
 					netfilter-persistent save
 				else
-			    for f in /etc/centos-release* ; do
-	      		if [[ -e $f ]] ; then
+					for f in /etc/centos-release* ; do
+						if [[ -e $f ]] ; then
 							iptables-save > /etc/iptables/rules.v4
 							ip6tables-save > /etc/iptables/rules.v6
 							break
@@ -274,7 +336,7 @@ USE CAUTION when saving iptables rules!  [y/n] > '
 					done
 				fi
 				# Remove fail2ban ipset rules from saved rules (these are added by fail2ban)
-				query='^-A INPUT -m set --match-set f2b-.* src -j REJECT --reject-with icmp-port-unreachable'
+				query='^.* -m set --match-set f2b-.* src -j REJECT --reject-with icmp-port-unreachable'
 				for file in rules.v4 rules.v6 ; do
 					if [[ -f "/etc/iptables/$file" ]] ; then
 						sed -i "/$query/d" "/etc/iptables/$file"
